ef54685f472ac152bf2e0792af5454dfdf2b4b35
hxerimione <purejang98@gmail.com>
2023-02-09 16:23:38 +0900

    Roman to Integer

--- src/main/java/codingtest/leetcode/RomanToInteger/README.md
+++ src/main/java/codingtest/leetcode/RomanToInteger/README.md
@@ -10,10 +10,18 @@
 왜냐하면 4는 5(V)의 1(I)을 뺀 수이기 때문이다. IX로 표기된 숫자 9도 같은 원리가 적용된다.
 
 ### 성능
-solution : runtime 6ms , memory : 43MB
+solution1 : runtime 6ms , memory : 43MB
 
 hashmap에 로마자와 대응하는 숫자값을 넣어준다.
 
 문자열의 끝부터 처음까지 순서대로 대응되는 값을 찾는데,
 
 왼쪽에 있는 값이 오른쪽보다 작다면 빼주고 크다면 더해주는 방식으로 구현하였다.
+
+solution2 : runtime 4ms , memory 42.6MB
+
+solution1에서 hashMap 사용한 부분을 change함수로 재구현
+
+hashMap을 사용했을 때보다 runtime이 빨라지는 이유를 아직 정확히는 모르겠다. 
+
+더 공부해보고 알게되면 추가할 예정
--- src/main/java/codingtest/leetcode/RomanToInteger/RomanToInteger.java
+++ src/main/java/codingtest/leetcode/RomanToInteger/RomanToInteger.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class RomanToInteger {
-    public int romanToInt(String s) {
+    public int romanToInt1(String s) {
         Map<Character,Integer> map = new HashMap<>();
         int output=0;
         map.put('I', 1);
@@ -25,4 +25,27 @@ public class RomanToInteger {
 
         return output;
     }
+    public int change(char c){
+        if (c=='I')return 1;
+        else if (c=='V') return 5;
+        else if (c=='X') return 10;
+        else if (c=='L') return 50;
+        else if (c=='C') return 100;
+        else if (c=='D') return 500;
+        else return 1000;
+    }
+    public int romanToInt2(String s) {
+        int output = 0;
+        output = change(s.charAt(s.length()-1));
+
+        for(int i = s.length() - 2; i >= 0; i--) {
+            if(change(s.charAt(i)) >= change(s.charAt(i + 1)))
+                output += change(s.charAt(i));
+            else
+                output -= change(s.charAt(i));
+        }
+
+        return output;
+    }
+
 }
